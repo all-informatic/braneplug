ac4f021a325b114fabb53b91b14ab6b9158fb6df
Olivier Lutzwiller <olivier.lutzwiller@sos-productions.com>
2019-12-22 20:44:45 +0100

    Linux compatible version with repositories capability and displays errors of installation   (solve issue no feedback after installing a plugin https://github.com/williamwilling/braneplug/issues/1)

--- zbplugin.lua
+++ zbplugin.lua
@@ -1,12 +1,13 @@
 return {
   description = "A plugin manager for ZeroBrane Studio.",
-  author = "William Willing",
-  version = 2,
+  author = "William Willing,Olivier Lutzwiller",
+  version = 6,
 
   install = function()
     local remotePath = "http://zerobranestore.blob.core.windows.net/braneplug/"
     download(remotePath .. "braneplug.lua", idePath .. "packages/braneplug.lua")
     download(remotePath .. "done.png", idePath .. "packages/done.png")
     download(remotePath .. "installing.png", idePath .. "packages/installing.png")
+	download(remotePath .. "warning.png", idePath .. "packages/warning.png")
   end
 }
